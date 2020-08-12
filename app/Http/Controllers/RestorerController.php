4a8eaf6670e577b3949cceab75f4e98a6bb2e48d
Vincent Chi <song374561@chivincent.net>
2020-08-12 13:16:12 +0800

    style: fix coding style.

--- app/Http/Controllers/RestorerController.php
+++ app/Http/Controllers/RestorerController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Services\DatabaseService;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 
 class RestorerController extends Controller
