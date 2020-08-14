df51ea1ad0e2388a3cae2e906e316f9d12540174
Vincent Chi <song374561@chivincent.net>
2020-08-14 09:33:50 +0800

    feat: design the base layout for service.

--- resources/js/App.tsx
+++ resources/js/App.tsx
@@ -1,8 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { makeStyles, createStyles } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import '../css/App.css';
+
+const useStyles = makeStyles(() => 
+    createStyles({
+        shortenInput: {
+            width: '75%',
+            height: '56px',
+        },
+        shortenButton: {
+            width: '25%',
+            height: '56px',
+        }
+    })
+)
 
 export default function App() {
+    const classes = useStyles();
+
     return (
-        <h1>Hello World</h1>
+        <React.Fragment>
+            <CssBaseline />
+            <div className="flex-center position-ref full-height">
+                <div className="content">
+                    <h1 className="title">Shorter URL</h1>
+                    <Box className="m-b-md">
+                        <TextField className={classes.shortenInput} variant="outlined" placeholder="Shorten your link"></TextField>
+                        <Button className={classes.shortenButton} variant="contained" color="primary">Shorten</Button>
+                    </Box>
+                </div>
+        </div>
+        </React.Fragment>
     );
 }
