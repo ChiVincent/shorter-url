494c81fdacda0317d52c5cf3bd26f35dc3c15e2b
Vincent Chi <song374561@chivincent.net>
2020-08-14 11:20:27 +0800

    feat: use api for shourten urls.

--- resources/js/App.tsx
+++ resources/js/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import { makeStyles, createStyles } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
@@ -26,12 +27,23 @@ function Shorten() {
     const [url, setUrl] = useState('');
 
     function handleShorternInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setIsShorted(false);
         setUrl(event.target.value);
     }
         
-    function handleShortenClick() {
-        setUrl('http://shorted.test/a1b2c3');
-        setIsShorted(true);
+    async function handleShortenClick() {
+        try {
+            const response = await axios.post('/api/', {url});
+
+            setUrl(response.data.data.shorted_url);
+            setIsShorted(true);
+        } catch (error) {
+            const notificationMessage = error.response
+                ? error.response.data?.message ?? '未預期的服務錯誤，請稍候再試'
+                : error.message ?? '未預期的系統錯誤，請檢查網路連線並稍候再試';
+
+            alert(notificationMessage);
+        }
     }
 
     function handleCopyClick() {
